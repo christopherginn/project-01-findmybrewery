8287475d2b87acc2c9790bf5ca1b087cae766a48
christopherginn <hey.im.chris@live.com>
2022-04-09 14:15:28 -0400

    Modal for DOB entry appears on click

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
 console.log("Hello");
+
+var enterBtnEl = $('#enterBtn');
+var modalEl = $('.modal');
+
+enterBtnEl.on("click", function () {
+  modalEl.addClass("is-active");
+
+  $(function (){
+      $('#datepicker').datepicker({
+          changeMonth: true,
+          changeYear: true
+      });
+  });
+});
+
+// var enterBtnEl = document.getElementById("#enterBtn");
+
+// enterBtnEl.addEventListener("click", function () {
+//   // alert("Works");
+// });
+
